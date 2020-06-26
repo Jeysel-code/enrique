2b0ad5141c16262c29d85c732a83d47dac2356a8
Jeysel-code <61567107+Jeysel-code@users.noreply.github.com>
2020-06-26 01:15:25 -0600

    Add files via upload

--- js/my-app.js
+++ js/my-app.js
@@ -29,16 +29,19 @@ var app = new Framework7({
     	url: 'profile.html',
     	name: 'profile',
   		},
+		/*HECHO POR JEYSEL*/
 		{
 		path: '/clases/',
     	url: 'clases.html',
     	name: 'clases',
   		},
+		/*HECHO POR JEYSEL*/
 		{
 		path: '/cursos/',
     	url: 'cursos.html',
     	name: 'cursos',
   		},
+		/*HECHO POR JEYSEL*/
 		{
 		path: '/info-g/',
     	url: 'info-g.html',
@@ -49,36 +52,43 @@ var app = new Framework7({
     	url: 'notas.html',
     	name: 'notas',
   		},
+		/*HECHO POR JEYSEL*/
 		{
 		path: '/contenido/',
     	url: 'contenido.html',
     	name: 'contenido',
   		},
+		/*HECHO POR JEYSEL*/
 		{
 		path: '/contenido-1/',
     	url: 'contenido-1.html',
     	name: 'contenido-1',
   		},
+		/*HECHO POR JEYSEL*/
 		{
 		path: '/recursos/',
     	url: 'recursos.html',
     	name: 'recursos',
   		},
+		/*HECHO POR JEYSEL*/
 		{
 		path: '/actividades/',
     	url: 'actividades.html',
     	name: 'actividades',
   		},
+		/*HECHO POR JEYSEL*/
 		{
 		path: '/bibliografia/',
     	url: 'bibliografia.html',
     	name: 'bibliografia',
   		},
+		/*HECHO POR JEYSEL*/
 		{
 		path: '/foro/',
     	url: 'foro.html',
     	name: 'foro',
   		},
+		/*HECHO POR JEYSEL*/
 		{
 		path: '/foro-1/',
     	url: 'foro-1.html',
